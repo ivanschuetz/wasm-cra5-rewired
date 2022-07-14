7f6e5ac378d04a22042a9c7dac12205bf0c5fb32
Ivan Schuetz <ivanschuetz@gmail.com>
2022-07-14 18:25:45 +0200

    Apply patch https://gist.github.com/hamza1311/35af4820d12f1d51d886b72455ae9b52

--- js/config-overrides.js
+++ js/config-overrides.js
@@ -1,22 +1,36 @@
+/* config-overrides.js */
+
 const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = function override(config, env) {
   const wasmExtensionRegExp = /\.wasm$/;
-
   config.resolve.extensions.push(".wasm");
 
-  config.module.rules.forEach((rule) => {
-    (rule.oneOf || []).forEach((oneOf) => {
-      if (oneOf.type === "asset/resource") {
-        // make file-loader ignore WASM files
-        oneOf.exclude.push(wasmExtensionRegExp);
-      }
-    });
-  });
+  //do stuff with the webpack config...
+  if (!Array.isArray(config.plugins)) {
+    config.plugins = [];
+  }
+
+  const crateDirectory = path.resolve(__dirname, "../wasm");
+  config.plugins.push(
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, "../wasm"),
+      outDir: path.resolve(__dirname, "src/pkg"),
+    })
+  );
 
   config.experiments = {
     asyncWebAssembly: true,
   };
+
+  config.module.rules.forEach((rule) => {
+    (rule.oneOf || []).forEach((oneOf) => {
+      if (oneOf.type === "asset/resource") {
+        oneOf.exclude.push(wasmExtensionRegExp);
+      }
+    });
+  });
 
   return config;
 };
--- js/src/App.js
+++ js/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 
-const wasmPromise = import("wasm");
+import { hello } from './pkg'
 
 function App() {
   return (
     <div className="App">
       <button
         onClick={async () => {
-          const { hello } = await wasmPromise;
           const res = await hello();
           console.log("wasm said: " + res);
         }}
